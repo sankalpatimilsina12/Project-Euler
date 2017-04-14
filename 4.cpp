7537ff86064613a140ea457f0dab1e9d9d86dd02
sankalpatimilsina12 <sankalpatimilsina12@gmail.com>
2017-04-14 23:14:59 +0545

    Question Fourth.

---
+++ 4.cpp
@@ -0,0 +1,47 @@
+/*
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+#include <iostream>
+
+bool isPalindrome(const int);
+
+int main()
+{
+  int largestPalindrome = 0;
+
+  for(int i = 100; i <= 999; i++)
+  {
+    for(int j = i; j <= 999; j++)
+    {
+      const int product = i * j; 
+
+      if(product > largestPalindrome && isPalindrome(product))
+      {
+        largestPalindrome = i * j;
+      }
+    }
+  }
+
+  std::cout << "The largest palindrome is: " << largestPalindrome << std::endl;
+  return 0;
+}
+
+bool isPalindrome(const int originalNum)
+{
+
+  int 
+    x          = originalNum,
+    reverseNum = 0;
+
+  while(x >= 1)
+  {
+    reverseNum = (reverseNum * 10) + (x % 10);
+    x /= 10;
+  }
+
+  return (reverseNum == originalNum);
+}
