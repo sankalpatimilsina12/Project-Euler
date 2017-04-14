e295ab1a99e56b613cf3fccd0cb51cf6259495ae
sankalpatimilsina12 <sankalpatimilsina12@gmail.com>
2017-04-14 21:22:50 +0545

    Question Third.

---
+++ 3.cpp
@@ -0,0 +1,42 @@
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+#include <iostream>
+#include <cmath>
+
+bool checkPrime(int);
+
+int main()
+{
+  const long int NUM = 600851475143;
+
+  int largestPrimeFactor = 0;
+
+  for(int i = 2; i <= sqrt(NUM); i++)
+  {
+
+    if(NUM % i == 0 and checkPrime(i))
+    {
+      largestPrimeFactor = i;
+    }
+
+  }
+
+  std::cout << "The largest prime factor of " << NUM << " is: " << largestPrimeFactor << std::endl;
+  return 0;
+}
+
+bool checkPrime(int i)
+{
+  for(int j = 2; j <= i/2; j++)
+  {
+    if(i % j == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
