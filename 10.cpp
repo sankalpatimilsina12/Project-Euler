89784308954c891c901e3a5bd279c48bbe2fa277
sankalpatimilsina12 <sankalpatimilsina12@gmail.com>
2017-04-16 08:01:20 +0545

    Questino Tenth.

---
+++ 10.cpp
@@ -0,0 +1,39 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int);
+
+int main()
+{
+  const int LIMIT = 2000000;
+
+  long int sum = 0;
+
+  for(int i = 2; i < LIMIT; i++)
+  {
+    if(isPrime(i))
+    {
+      sum += i;
+    }
+  }
+
+  std::cout << "The sum is: " << sum << std::endl;
+  return 0;
+}
+
+bool isPrime(int x)
+{
+  for(int i = 2; i <= sqrt(x); i++)
+  {
+    if(x % i == 0)
+      return false;
+  }
+
+  return true;
+}
