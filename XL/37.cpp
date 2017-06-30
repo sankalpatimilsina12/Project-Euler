b4178c692eda91ae2c647129847920f09c314e72
sankalpatimilsina12 <sankalpatimilsina12@gmail.com>
2017-06-30 13:55:54 +0545

    37.cpp

---
+++ XL/37.cpp
@@ -0,0 +1,60 @@
+/**
+  The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove
+  digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work
+  from right to left: 3797, 379, 37, and 3.
+
+  Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+  NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+*/
+
+#include <iostream>
+#include <cmath>
+
+int getLength(long);
+bool checkPrime(long);
+
+int main() {
+  int countPrimes = 0;
+  long sum = 0;
+
+  for(long i = 8; countPrimes != 11; i++) {
+    if(checkPrime(i)) {
+      int numLen = getLength(i);
+      int tempILeft = i, tempIRight = i;
+      while(--numLen) {
+        tempILeft = i % (long)pow(10, numLen);
+        tempIRight = i / (long)pow(10, numLen);
+
+        if(!checkPrime(tempILeft) or !checkPrime(tempIRight))
+          break;
+      }
+      if(!numLen) {
+        countPrimes++;
+        sum += i;
+      }
+    }
+  }
+
+  std::cout << "The sum is: " << sum << std::endl;
+  return 0;
+}
+
+int getLength(long num) {
+  int count = 0;
+  while(num) {
+    count++;
+    num /= 10;
+  }
+  return count;
+}
+
+bool checkPrime(long num) {
+  if(num == 0 or num == 1)
+    return false;
+  for(long i = 2; i <= sqrt(num); i++)
+    if(num % i == 0)
+      return false;
+  return true;
+}
+
